9ef735470ce1ecc76a7c219b48b61aeef2771206
Laomao Wang <tr1000tre@gmail.com>
2021-01-07 00:21:25 +0800

    third push

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ const bot = linebot({
     // channelId: CHANNEL_ID,
     // channelSecret: CHANNEL_SECRET,
     // channelAccessToken: CHANNEL_ACCESS_TOKEN
-    channelId: "1655554384",
-    channelSecret: "7d99742ecd726beea928c780f7491a20",
-    channelAccessToken: "2HkvdXHtgKmt+wQbiZrcktDU41OG6VUDmJrFeocoBvbNmanbg8Z9entPRfzTQ/wMU1OosIzb+PdYMAf8BVrb30Q3GBcJO9YfWSpML/549ZVWyHI10mJcEVzare8ohgEJDU6/p+IGPHdYpzlQ41XHlwdB04t89/1O/w1cDnyilFU="
-});
-
-//收到LINE訊息後印出
-bot.on('message', function(event) {
-    console.log(event);
+    channelId: '1655554384',
+    channelSecret: '7d99742ecd726beea928c780f7491a20',
+    channelAccessToken: '2HkvdXHtgKmt+wQbiZrcktDU41OG6VUDmJrFeocoBvbNmanbg8Z9entPRfzTQ/wMU1OosIzb+PdYMAf8BVrb30Q3GBcJO9YfWSpML/549ZVWyHI10mJcEVzare8ohgEJDU6/p+IGPHdYpzlQ41XHlwdB04t89/1O/w1cDnyilFU='
 });
 
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
 
-//預設port3000
-// app.listen(3000);
+bot.on('message', function(event) {
 
-//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
-var server = app.listen(process.env.PORT || 8080, function() {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+    event.reply(event.message.text).then(function(data) {
+        console.log("reply successfully");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.listen('/linewebhook', 3000, function () {
+    console.log('[BOT已準備就緒]');
+});
