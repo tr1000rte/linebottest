dc3d6deb511aeba9525ba124aa9d9b45bcd2f94c
Laomao Wang <tr1000tre@gmail.com>
2021-01-08 18:20:59 +0800

    officalreply13 push

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require('express');
 const line = require('@line/bot-sdk');
 const app = express();
 
+new line.Client(config);
+line.middleware(config);
+
 const config = {
     channelAccessToken: '2HkvdXHtgKmt+wQbiZrcktDU41OG6VUDmJrFeocoBvbNmanbg8Z9entPRfzTQ/wMU1OosIzb+PdYMAf8BVrb30Q3GBcJO9YfWSpML/549ZVWyHI10mJcEVzare8ohgEJDU6/p+IGPHdYpzlQ41XHlwdB04t89/1O/w1cDnyilFU=',
     channelSecret: '7d99742ecd726beea928c780f7491a20'
   };
 
 
-  app.post('/webhook', (req, res) => {
+  app.post('/webhook', line.middleware(config), (req, res) => {
     Promise
       .all(req.body.events.map(handleEvent))
       .then((result) => res.json(result));
