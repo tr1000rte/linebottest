b5929cb95ebf7dca5b6458c67e80a9674d2a9483
Laomao Wang <tr1000tre@gmail.com>
2021-01-08 17:21:34 +0800

    officalreply2 push

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const line = require('@line/bot-sdk');
+const app = express();
 
+const config = {
+    channelAccessToken: '2HkvdXHtgKmt+wQbiZrcktDU41OG6VUDmJrFeocoBvbNmanbg8Z9entPRfzTQ/wMU1OosIzb+PdYMAf8BVrb30Q3GBcJO9YfWSpML/549ZVWyHI10mJcEVzare8ohgEJDU6/p+IGPHdYpzlQ41XHlwdB04t89/1O/w1cDnyilFU=',
+    channelSecret: '7d99742ecd726beea928c780f7491a20'
+  };
 
-require('dotenv').config();
-
-const client = new line.Client({
-    channelAccessToken: '2HkvdXHtgKmt+wQbiZrcktDU41OG6VUDmJrFeocoBvbNmanbg8Z9entPRfzTQ/wMU1OosIzb+PdYMAf8BVrb30Q3GBcJO9YfWSpML/549ZVWyHI10mJcEVzare8ohgEJDU6/p+IGPHdYpzlQ41XHlwdB04t89/1O/w1cDnyilFU='
-  });
+const client = new line.Client(config);
 
 const message = {
-     type: 'text',
+    type: 'text',
     text: 'Hello World!'
 };
 
@@ -21,15 +22,25 @@ client.replyMessage('<replyToken>', message)
     console.log(err);
   });
 
+  app.post('/', line.middleware(config), (req, res) => {
+    Promise
+      .all(req.body.events.map(handleEvent))
+      .then((result) => res.json(result));
+  });
 
-const app = express();
-const linebotParser = bot.parser();
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      return Promise.resolve(null);
+    }
+  
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: event.message.text
+    });
+}
 
 
-
-
-app.post('/', linebotParser);
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 8080, () => {
     console.log('BOT已準備就緒');
 });
 
